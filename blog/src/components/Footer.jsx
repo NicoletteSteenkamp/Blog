080b70966bb6ea0b62afecf5591c92569ff99756
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-11-06 12:47:22 +0200

    installed dependencies

---
+++ blog/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+import Logo from "../assets/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made by ♥️ Nicolette and Koli ♥️ with <b>React.js♥️</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
